e9319189fe06641af13b3e80ea1e755b0343c433
Haohahahaha <1259203802@qq.com>
2023-12-13 08:10:36 +0800

    2023-12-13_08:10:36 update

--- 2-cfw.sh
+++ 2-cfw.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 # install Clash-for-Windows
-cd ~/Documents/
-# wget https://github.com/Fndroid/clash_for_windows_pkg/releases/download/0.20.36/Clash.for.Windows-0.20.36-x64-linux.tar.gz
-tar zxvf ./Clash.for.Windows-0.20.36-x64-linux.tar.gz
-./Clash\ for\ Windows-0.20.36-x64-linux/cfw &
+(cd ~/Documents/ && 
+wget https://github.com/Haohahahaha/cs/raw/master/docs/greenhand/Ubuntu2204-greenhand/Clash.for.Windows.Setup.0.20.39.ia32.exe &&
+tar zxvf ./Clash.for.Windows-0.20.36-x64-linux.tar.gz &&
+./Clash\ for\ Windows-0.20.36-x64-linux/cfw &)
