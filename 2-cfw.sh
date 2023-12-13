f91281878b6fa2204babb5fd0aa6e0dfbfc160c1
Haohahahaha <1259203802@qq.com>
2023-12-13 08:34:47 +0800

    2023-12-13_08:34:47 update

--- 2-cfw.sh
+++ 2-cfw.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 # install Clash-for-Windows
-(cd ~/Documents/ && 
-wget https://github.com/Haohahahaha/cs/raw/master/docs/greenhand/Ubuntu2204-greenhand/Clash.for.Windows.Setup.0.20.39.ia32.exe &&
-tar zxvf ./Clash.for.Windows-0.20.36-x64-linux.tar.gz &&
-./Clash\ for\ Windows-0.20.36-x64-linux/cfw &)
+# (cd ~/Documents/ && 
+# wget https://github.com/Haohahahaha/cs/raw/master/docs/greenhand/Ubuntu2204-greenhand/Clash.for.Windows.Setup.0.20.39.ia32.exe &&
+# tar zxvf ./Clash.for.Windows-0.20.36-x64-linux.tar.gz &&
+# ./Clash\ for\ Windows-0.20.36-x64-linux/cfw &)
+
+echo "请参照教程在 Windows 下安装好 CfW 后，在虚拟机内设置代理。此脚本已无法使用。"
